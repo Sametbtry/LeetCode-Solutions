0018126ea05fc9591ba88a72c13be1ce587ff510
Sametbtry <careersamet@gmail.com>
2026-02-26 12:32:38 +0300

    1068. Product Sales Analysis I eklendi
    commit mesajlari icin deneme
    bir sey bir seyler

--- SQL/basic_Joins.sql
+++ SQL/basic_Joins.sql
@@ -2,3 +2,8 @@
 -- Write your PostgreSQL query statement below
 select e_uni.unique_id, e.name from Employees e
 left join EmployeeUNI e_uni on e.id = e_uni.id;
+
+-- 1068. Product Sales Analysis I
+-- Write your PostgreSQL query statement below
+select p.product_name, s.year, s.price  from Sales s
+left join Product p USING (product_id);  
