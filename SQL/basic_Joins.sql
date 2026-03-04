2964f462c3e64ffec785b6d534af043b3b708ed7
Sametbtry <careersamet@gmail.com>
2026-03-04 17:32:25 +0300

    581. Customer Who Visited but Did Not Make Any Transactions, eklendi

--- SQL/basic_Joins.sql
+++ SQL/basic_Joins.sql
@@ -7,3 +7,34 @@ left join EmployeeUNI e_uni on e.id = e_uni.id;
 -- Write your PostgreSQL query statement below
 select p.product_name, s.year, s.price  from Sales s
 left join Product p USING (product_id);  
+
+'''
+1581. Customer Who Visited but Did Not Make Any Transactions
+LEFT JOIN + IS NULL:
+NOT EXISTS:
+NOT IN:
+https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/solutions/6699097/what-to-choose-explanation-for-not-in-no-c4tc
+The best option is usually the NOT EXISTS approach.
+A close second (slightly worse) is LEFT JOIN + IS NULL.
+The worst option is NOT IN.
+ '''
+-- Write your PostgreSQL query statement below
+select v.customer_id , count(customer_id) as count_no_trans  from Visits v
+left join Transactions t on v.visit_id = t.visit_id
+where transaction_id is null  
+group by v.customer_id;
+
+-- Write your PostgreSQL query statement below
+ SELECT v.customer_id
+     , count(v.customer_id) count_no_trans
+  FROM visits v
+ WHERE NOT EXISTS (SELECT 1 FROM transactions t WHERE v.visit_id = t.visit_id)
+ GROUP BY v.customer_id;
+ 
+-- Write your PostgreSQL query statement below
+ SELECT v.customer_id
+     , count(v.customer_id) count_no_trans
+  FROM visits v
+ WHERE v.visit_id NOT IN (SELECT visit_id FROM transactions)
+ GROUP BY v.customer_id;
+ 
